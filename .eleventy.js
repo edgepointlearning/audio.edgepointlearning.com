824a399f07457fb05d0132a175f4f7fbd42536a4
tylercampbell <tylercampbell@users.noreply.github.com>
2022-02-07 03:04:19 -0500

    switch to tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,13 @@ module.exports = function(eleventyConfig) {
   
   // Copy '/src/assets/' to site root
   eleventyConfig.addPassthroughCopy({'./src/assets': '/'});
-
+  // eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   // Set input and output directories
   return {
     dir: {
-      input: 'src/index.html',
-      output: '_site'
+      input: 'src/*.html',
+      output: 'public'
     }
   }
 };
