1002959002cb2b2d5138b54eff2419036dc9685c
Tyler Campbell <tylercampbell@users.noreply.github.com>
2022-02-11 11:53:00 -0500

    Svg sprite test (#5)
    * svg sprite test
    
    * cleanup & comment
    
    * add eleventy-plugin-svg-sprite
    
    * tailwind cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
 const metagen = require('eleventy-plugin-metagen');
+const svgSprite = require("eleventy-plugin-svg-sprite");
 
 module.exports = function(eleventyConfig) {
+
   eleventyConfig.addPassthroughCopy({'./src/static': '/'});
+
+  eleventyConfig.addWatchTarget('./tailwind.config.js');
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
+  
   eleventyConfig.addPlugin(metagen);
+  
+  eleventyConfig.addPlugin(svgSprite, {
+    path: "./src/assets/svg",
+  });
+
   return {
     dir: {
       input: 'src/*.html',
